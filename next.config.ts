8ed8e71837e10f810e1dfbece1cb4aadbd04dc68
Muhammad Hashir Malik <m.hashir.malik1@gmail.com>
2026-07-23 06:39:59 +0500

    tmp

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,25 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+
   allowedDevOrigins: [
-    '192.168.10.1',
-    '192.168.10.2',
-    '192.168.10.3',
-    '192.168.10.4',
-    '192.168.10.5',
-    '192.168.10.6',
-    '192.168.10.7',
-    '192.168.10.8',
-    '192.168.10.9',
-    '192.168.0.109',
-  ]
+    "192.168.10.1",
+    "192.168.10.2",
+    "192.168.10.3",
+    "192.168.10.4",
+    "192.168.10.5",
+    "192.168.10.6",
+    "192.168.10.7",
+    "192.168.10.8",
+    "192.168.10.9",
+    "192.168.0.109",
+  ],
 };
 
 export default nextConfig;
